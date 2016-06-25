dc7af743a48f354e9b0957f890ca61e25a0e6320
sjwabc <sjw_228@163.com>
2016-06-25 17:15:22 +0800

    更改lib路径

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 /**
  * Created by sujiawei on 2016/6/24.
  */
+
+require.config({
+    baseUrl : "js",
+    paths : {
+        "jquery" : "lib/jquery/jquery-2.2.3",
+        "angular" : "angular/angular.min",
+        "bootstrap" : "bootstrap/bootstrap.min"
+    }
+});
+
+
+require('jquery',function($){
+    $("")
+});
